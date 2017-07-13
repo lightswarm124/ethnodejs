5e5b921b20e3bdeeea612b8c69f91f3521c13fad
lightswarm124 <lightswarm@gmail.com>
2017-07-13 17:56:54 -0400

    add more gas

--- truffle.js
+++ truffle.js
@@ -4,7 +4,7 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*", // Match any network id
-      gas: 8712388,
+      gas: 8712388421,
       gasPrice: 10000000000
     }
   }
